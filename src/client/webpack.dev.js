303eff29193a9cb7a93826b0b967c75c9766f608
Grzegorz Czajkowski <czajkowski.grz@gmail.com>
2022-08-08 23:09:31 +0200

    update index.html file location

--- src/client/webpack.dev.js
+++ src/client/webpack.dev.js
@@ -1,14 +1,19 @@
 const { merge } = require('webpack-merge')
 const common = require('./webpack.common.js')
-const path = require('path');
+const path = require('path')
 
 module.exports = merge(common, {
     mode: 'development',
     devtool: 'eval-source-map',
     devServer: {
-        static: {
-            directory: path.join(__dirname, '../../dist/client'),
-        },
+        static: [
+            {
+                directory: path.join(__dirname, '../../dist/client'),
+            },
+            {
+                directory: path.join(__dirname, '../../'),
+            },
+        ],
         hot: true,
     },
 })
